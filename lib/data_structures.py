a721f9ad4bf2d2eb3a668aba7ef2a432212af977
wills blake <richwblake@gmail.com>
2022-08-02 11:30:12 -0700

    Automated push

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,13 +17,14 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     pass
@@ -33,3 +34,8 @@ def print_spiciest_foods(spicy_foods):
 
 def get_average_heat_level(spicy_foods):
     pass
+
+def sort_by_heat(spicy_foods):
+    pass
+
+
